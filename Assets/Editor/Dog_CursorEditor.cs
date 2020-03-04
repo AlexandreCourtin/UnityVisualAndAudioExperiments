cac93c83f970d08ca22f11eb02d3bfa8951143f9
acourtin <acourtin@student.42.fr>
2020-03-04 17:19:52 +0100

    (dog) added cursor debug

---
+++ Assets/Editor/Dog_CursorEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(Dog_Cursor))]
+public class Dog_CursorEditor : Editor
+{
+    void OnSceneGUI() {
+        Dog_Cursor cursor = (Dog_Cursor)target;
+
+        Handles.color = Color.red;
+		Handles.DrawWireCube(cursor.mousePosition, new Vector3(1f, 1f, 1f));
+    }
+}
