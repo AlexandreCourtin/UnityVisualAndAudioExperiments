1274795434ccf103443a7aca1fea6413d99e2e4a
acourtin <acourtin@student.42.fr>
2020-03-05 23:12:55 +0100

    lots of hotfixes

--- Assets/Scripts/vfxmusic_one.cs
+++ Assets/Scripts/vfxmusic_one.cs
@@ -7,26 +7,24 @@ public class vfxmusic_one : MonoBehaviour
 {
     public int sampleId;
 
-    AudioSource music;
+    Dog_Music music;
     VisualEffect vfx;
     float power;
-    float[] samples = new float[512];
     float currentPower;
 
     void Start() {
-        music = GameObject.Find("Music").GetComponent<AudioSource>();
+        music = GameObject.Find("Music").GetComponent<Dog_Music>();
         vfx = GetComponent<VisualEffect>();
         power = 2.5f;
     }
 
     void Update() {
-        music.GetSpectrumData(samples, 0, FFTWindow.Blackman);
-        float nextPower = samples[sampleId] * power * sampleId * .5f;
+        float nextPower = music.samples[sampleId] * power * sampleId * .5f;
 
         if (nextPower > currentPower) {
             currentPower = nextPower;
         } else if (currentPower > 0f) {
-            currentPower -= Time.deltaTime * currentPower * 2f;
+            currentPower = Mathf.Clamp(currentPower - Time.deltaTime * currentPower * 2f, 0f, 1000f);
         }
 
         vfx.SetFloat("Sample", currentPower);
