7174bf9fe1ccdafe06f95ccde44e852fc17077f8
acourtin <acourtin@student.42.fr>
2020-03-08 15:07:25 +0100

    (dog) good boy is now fetching the ball

--- Assets/Scripts/Dog_Ball.cs
+++ Assets/Scripts/Dog_Ball.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class Dog_Ball : MonoBehaviour
 {
     public bool isControllingBall;
+    public bool isInDogMouth;
 
     Dog_Cursor cursor;
     Rigidbody ballRb;
 
     void Start() {
         isControllingBall = false;
+        isInDogMouth = false;
         cursor = GameObject.Find("Camera").GetComponent<Dog_Cursor>();
         ballRb = GetComponent<Rigidbody>();
     }
 
     void Update() {
+        if (isInDogMouth) {
+            ballRb.useGravity = false;
+            ballRb.velocity = Vector3.zero;
+            transform.position = GameObject.Find("BallJawPos").transform.position;
+        }
+
         if (Input.GetMouseButton(0)) {
+            isControllingBall = true;
+            isInDogMouth = false;
             ballRb.useGravity = false;
             ballRb.velocity = Vector3.zero;
             transform.position = cursor.mousePosition;
         } else if (Input.GetMouseButtonUp(0)) {
+            isControllingBall = false;
             ballRb.useGravity = true;
             ballRb.velocity = Vector3.zero;
             ballRb.AddForce(Camera.main.transform.forward * 1000f + Vector3.up * 1000f);
--- Assets/Scripts/Dog_Controller.cs
+++ Assets/Scripts/Dog_Controller.cs
@@ -5,33 +5,52 @@ using UnityEngine;
 public class Dog_Controller : MonoBehaviour
 {
     public float rotSpeed = 1f;
+    public float speed = 2f;
+    public float distanceToBall = 1f;
 
     Dog_Cursor cursor;
     GameObject ball;
     Dog_Ball dogball;
+    bool hasBall;
 
     void Start() {
         cursor = GameObject.Find("Camera").GetComponent<Dog_Cursor>();
         ball = GameObject.Find("DogBall");
         dogball = ball.GetComponent<Dog_Ball>();
+        hasBall = false;
     }
 
     void FixedUpdate() {
-        // DogLookAt(ball.transform.position);
-        if (!dogball.isControllingBall) {
+        if (hasBall && Vector3.Distance(transform.position, ball.transform.position) > distanceToBall) {
+            hasBall = false;
+        }
+        else if (!dogball.isControllingBall && Vector3.Distance(transform.position, ball.transform.position) <= distanceToBall && !hasBall) {
+            hasBall = true;
+            dogball.isInDogMouth = true;
+        }
+        else if (!dogball.isControllingBall && Vector3.Distance(transform.position, ball.transform.position) > distanceToBall && !hasBall) {
             DogMovePosition(ball.transform.position);
-        } else {
+            DogLookAt(ball.transform.position);
+        } else if (dogball.isControllingBall) {
             DogMovePosition(Vector3.zero);
+            DogLookAt(ball.transform.position);
+        } else if (hasBall) {
+            DogMovePosition(Vector3.zero);
+            DogLookAt(Camera.main.transform.position);
         }
     }
 
     private void DogMovePosition(Vector3 target) {
-        DogLookAt(target);
-        transform.position = Vector3.Lerp(transform.position, target, Time.fixedDeltaTime * 2f);
+        Vector3 targetPosition = new Vector3(target.x, transform.position.y, target.z);
+        float t = Time.fixedDeltaTime * speed;
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, t);
+        // transform.position = Vector3.Lerp(transform.position, targetPosition, t);
     }
 
     private void DogLookAt(Vector3 target) {
         Vector3 lookDirection = new Vector3(target.x, transform.position.y, target.z);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(transform.position - lookDirection, Vector3.up), rotSpeed * Time.fixedDeltaTime);
+        Quaternion q = Quaternion.LookRotation(transform.position - lookDirection, Vector3.up);
+        float t = Time.fixedDeltaTime * rotSpeed;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, t);
     }
 }
