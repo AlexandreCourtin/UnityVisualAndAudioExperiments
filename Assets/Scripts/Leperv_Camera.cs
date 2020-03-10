f240a11a0bb6895ce297314c18bc6858a3aea4a7
acourtin <acourtin@student.42.fr>
2020-03-10 16:03:10 +0100

    (dog) changed skull model + camera follows mouse

---
+++ Assets/Scripts/Leperv_Camera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leperv_Camera : MonoBehaviour
+{
+    void Update() {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(mouseRay, out hit, Mathf.Infinity)) {
+            transform.eulerAngles = new Vector3(-hit.point.y * .5f, hit.point.x * .5f, 0f);
+            // mousePosition = Vector3.Lerp(mousePosition, hit.point, reactionTime * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/Leperv_Generator.cs
+++ Assets/Scripts/Leperv_Generator.cs
@@ -9,7 +9,7 @@ public class Leperv_Generator : MonoBehaviour
     void Start() {
         for (int i = 1 ; i < 51 ; i++) {
             float radius = .2f;
-            Vector3 pos = new Vector3(radius * Mathf.Sin(i * 6.9f * Mathf.Deg2Rad), radius * Mathf.Cos(i * 6.9f * Mathf.Deg2Rad), 0f);
+            Vector3 pos = new Vector3(radius * Mathf.Sin(i * 6.9f * Mathf.Deg2Rad), radius * Mathf.Cos(i * 6.9f * Mathf.Deg2Rad), 3.5f);
             GameObject c = Instantiate(lpCube, pos, Quaternion.identity);
             c.transform.eulerAngles = new Vector3(0f, 0f, -i * 6.9f);
             c.GetComponent<Leperv_Cube>().sampleId = i;
