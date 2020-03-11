9337e020a54c1c41a60dccfb202b5a93705cb408
acourtin <acourtin@student.42.fr>
2020-03-11 14:16:01 +0100

    hotfixes for leperv and funny dog

--- Assets/Scripts/Leperv_Generator.cs
+++ Assets/Scripts/Leperv_Generator.cs
@@ -7,12 +7,13 @@ public class Leperv_Generator : MonoBehaviour
     public GameObject lpCube;
 
     void Start() {
-        for (int i = 1 ; i < 51 ; i++) {
+        for (int i = 0 ; i < 50 ; i++) {
             float radius = .2f;
-            Vector3 pos = new Vector3(radius * Mathf.Sin(i * 6.9f * Mathf.Deg2Rad), radius * Mathf.Cos(i * 6.9f * Mathf.Deg2Rad), 3.5f);
+            float rot = 7.5f;
+            Vector3 pos = new Vector3(radius * Mathf.Sin(i * rot * Mathf.Deg2Rad), radius * Mathf.Cos(i * rot * Mathf.Deg2Rad), 3.5f);
             GameObject c = Instantiate(lpCube, pos, Quaternion.identity);
-            c.transform.eulerAngles = new Vector3(0f, 0f, -i * 6.9f);
-            c.GetComponent<Leperv_Cube>().sampleId = i;
+            c.transform.eulerAngles = new Vector3(0f, 0f, -i * rot);
+            c.GetComponent<Leperv_Cube>().sampleId = i + 3;
         }
     }
 }
